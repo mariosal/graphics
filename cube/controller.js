1bdfd456d083f65c2d667ea6033faa1c108099bb
Mario Saldinger <mariosaldinger@gmail.com>
2012-06-12 18:54:31 -0400

    Revert to WebGL cube

--- cube/controller.js
+++ cube/controller.js
@@ -1,10 +1,7 @@
-var dx = 0, dy = 0;
-
 document.onmousewheel = function ( e ) {
     var d = e.wheelDeltaY / 1000;
-    if ( cube.position.z + d >= NEAR && cube.position.z + d <= FAR - 1 ) {
-        cube.position.z += d;
-        console.log( cube.position.z );
+    if ( cube.z + d > -15 && cube.z + d < -5 ) {
+        cube.setZoom( cube.z + d );
     }
     return false;
 };
@@ -12,10 +9,7 @@ document.onmousedown = function ( e ) {
     if ( e.which == 1 ) {
         mouse = [ e.clientX, e.clientY ];
         document.onmousemove = function ( e ) {
-            dx += ( e.clientY - mouse[ 1 ] ) / 50;
-            dy += ( e.clientX - mouse[ 0 ] ) / 50;
-            cube.rotation.set( dx, dy, 0 );
-
+            cube.rotate( e.clientY - mouse[ 1 ], e.clientX - mouse[ 0 ], 0 );
             mouse = [ e.clientX, e.clientY ];
         };
     }
---
+++ cube/fragment.c
@@ -0,0 +1,7 @@
+precision mediump float;
+
+varying vec4 fragmentColor;
+
+void main( void ) {
+    gl_FragColor = fragmentColor;
+}
