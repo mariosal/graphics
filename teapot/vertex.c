91f1e791297c561de38b91054d9a26f26e74543e
Mario Saldinger <mariosaldinger@gmail.com>
2012-07-02 04:52:13 -0400

    WebGL Teapot, no lights

---
+++ teapot/vertex.c
@@ -0,0 +1,9 @@
+attribute vec3 vertex;
+
+uniform mat4 projection;
+uniform mat4 zoom;
+uniform mat4 rotation;
+
+void main( void ) {
+    gl_Position = projection * zoom * rotation * vec4( vertex, 1.0 );
+}
